45e434c052f82de2acd720eeb2d2193636087eee
Lacen <43798572+Lacen@users.noreply.github.com>
2018-10-21 10:52:28 -0600

    Visual Studio Files
    contains all the Files for Visual studio (excluding solution)

---
+++ Cmpt395/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cmpt395.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Cmpt395/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Cmpt395.Models
+{
+    //Login Model to store password and Email
+    public class LoginModel
+    {
+        
+        public string Email { get; set; }
+        
+        public string Password { get; set; }
+       
+    }
+}
