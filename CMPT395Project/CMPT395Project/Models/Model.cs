e1dbde4b587ce117565f1d935af158faa4585976
GrahamCooper <GrahamCooper@DESKTOP-GD918O0>
2018-10-30 21:31:03 -0600

    modified model

--- CMPT395Project/CMPT395Project/Models/Model.cs
+++ CMPT395Project/CMPT395Project/Models/Model.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 // ttps://docs.microsoft.com/en-us/ef/core/get-started/aspnetcore/new-db?tabs=visual-studio
 namespace CMPT395Project.Models
@@ -10,7 +12,7 @@ namespace CMPT395Project.Models
         public ProjectContext(DbContextOptions<ProjectContext> options)
             : base(options)
         { }
-
+        
         public DbSet<Employee> Employee { get; set; }
         public DbSet<Company> Company { get; set; }
         public DbSet<Contractor> Contractor { get; set; }
@@ -21,6 +23,7 @@ namespace CMPT395Project.Models
 
     public class Employee
     {
+        [Key]
         public int EmployeeId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -31,8 +34,11 @@ namespace CMPT395Project.Models
 
     public class Employee_Hours
     {
+        [Key]
         public int TimeSheetId { get; set; }
+        [ForeignKey("Contracts")]
         public int ContractId { get; set; }
+        public virtual Contracts Contracts { get; set; }
         public int Year { get; set; }
         public int Month { get; set; }
         public int CurrentMonth { get; set; }
@@ -42,56 +48,66 @@ namespace CMPT395Project.Models
 
     public class Admin
     {
+        [Key]
         public int AdminId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string password { get; set; }
+        public string Password { get; set; }
     }
 
     public class Company
     {
-        public int companyIds { get; set; }
-        public string companyName { get; set; }
+        [Key]
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
     }
 
     public class Contractor
     {
-        public int contractorId { get; set; }
-        public string contractorFirstName { get; set; }
-        public string contractorLastName { get; set; }
-        public int companyId { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
+        [Key]
+        public int ContractorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [ForeignKey("Company")]
+        public int CompanyId { get; set; }
+        public virtual Company Company { set; get; }
+        public string Email { get; set; }
+        public string Password { get; set; }
 
     }
 
     public class Contracts
     {
-        public int contractId { get; set; }
-        public int companyId { get; set; }
-        public int contractorId { get; set; }
+        [Key]
+        public int ContractId { get; set; }
+        [ForeignKey("Contractor")]
+        public int ContractorId { get; set; }
+        public virtual Contractor Contractor { set; get; }
+        [ForeignKey("Company")]
+        public int CompanyId { get; set; }
+        public virtual Company Company { get; set; }
 
-        public int p1CharRate { get; set; }
-        public int p1PayRate { get; set; }
-        public DateTime p1StartDate { get; set; }
-        public DateTime p1EndtDate { get; set; }
+        public int P1CharRate { get; set; }
+        public int P1PayRate { get; set; }
+        public DateTime P1StartDate { get; set; }
+        public DateTime P1EndtDate { get; set; }
 
-        public int p2CharRate { get; set; }
-        public int p2PayRate { get; set; }
-        public DateTime p2StartDate { get; set; }
-        public DateTime p2EndtDate { get; set; }
+        public int P2CharRate { get; set; }
+        public int P2PayRate { get; set; }
+        public DateTime P2StartDate { get; set; }
+        public DateTime P2EndtDate { get; set; }
 
-        public int p3CharRate { get; set; }
-        public int p3PayRate { get; set; }
-        public DateTime p3StartDate { get; set; }
-        public DateTime p3EndtDate { get; set; }
+        public int P3CharRate { get; set; }
+        public int P3PayRate { get; set; }
+        public DateTime P3StartDate { get; set; }
+        public DateTime P3EndtDate { get; set; }
 
-        public int p4CharRate { get; set; }
-        public int p4PayRate { get; set; }
-        public DateTime p4StartDate { get; set; }
-        public DateTime p4EndtDate { get; set; }
+        public int P4CharRate { get; set; }
+        public int P4PayRate { get; set; }
+        public DateTime P4StartDate { get; set; }
+        public DateTime P4EndtDate { get; set; }
 
-        public String renewal { get; set; }
-        public Char activeContract { get; set; }
+        public String Renewal { get; set; }
+        public Char ActiveContract { get; set; }
     }
 }
