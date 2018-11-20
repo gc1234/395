c2dfe6df17543d8a696d55dd498f5d6f1ccff39f
GrahamCooper <GrahamCooper@DESKTOP-GD918O0>
2018-11-20 00:59:20 -0700

    Modified tests.

--- CMPT395Project/CMPT395ProjectTests/ControllerTests/AdminsControllerTests.cs
+++ CMPT395Project/CMPT395ProjectTests/ControllerTests/AdminsControllerTests.cs
@@ -17,7 +17,40 @@ namespace CMPT395ProjectTests.ControllerTests
         [Fact]
         public async Task TestAdminsIndex()
         {
+            //Arrange
             var mockRepo = new Mock<IBrainstormSessionRepository>();
+            mockRepo.Setup(repo => repo.ListAsync())
+                .ReturnAsync(GetTestSessions());
+            var controller = new AdminsController(mockRepo.Object);
+
+            //Act
+            var result = await controller.Index(null);
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<StormSessionViewModel>>(
+                viewResult.ViewData.Model);
+            Assert.Equal(2, model.Count());
+        }
+
+        private List<BrainstormSession> GetTestSessions()
+        {
+            var sessions = new List<BrainstormSessions>();
+            sessions.Add(new BrainstormSessions()
+            {
+                AdminId = 1,
+                FirstName = "test",
+                LastName = "one",
+                Password = "test"
+            });
+            sessions.Add(new BrainstormSessions()
+            {
+                AdminId = 2,
+                FirstName = "test",
+                LastName = "two",
+                Password = "test"
+            });
+            return sessions;
         }
     }
 }
