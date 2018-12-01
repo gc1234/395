43a9a0f7645aac61290b9740c46873b2ad939b08
Anthony Wong <lacen_black@hotmail.com>
2018-11-30 20:33:00 -0700

    added a image background to index and created a basic framework of sql

--- CMPT395Project/CMPT395Project/Class/DatabaseConnect.cs
+++ CMPT395Project/CMPT395Project/Class/DatabaseConnect.cs
@@ -7,5 +7,41 @@ namespace CMPT395Project.Class
 {
     public class DatabaseConnect
     {
+        private static DatabaseConnect instance = new DatabaseConnect();
+        private string serverstring= "";
+        private SqlConnection con = null;
+        public DatabaseConnect(){
+           }
+
+        public static DatabaseConnect Instance
+        {
+            get {
+                return Instance;
+                }
+            }
+
+
+        public SqlConnection Connect()
+        {
+            if (con == null)
+            {
+                con = new SqlConnection(serverstring);
+                return con;
+            }
+            return con;
+        }
+        
+
+
+
+
+
+
+
+
     }
-}
+
+
+  
+    }
+
